68e572ca0300db2e6b3840a3913e2dd304f03b84
engdanilo <danilo.eng.mec@hotmail.com>
2023-04-11 00:21:19 -0300

    Correção

--- estetica/settings.py
+++ estetica/settings.py
@@ -134,6 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 ]"""
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
