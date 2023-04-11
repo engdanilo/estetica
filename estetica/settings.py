aafe3a2993f56aa7b86b6039621a7f115e056251
Danilo Santana <51862854+engdanilo@users.noreply.github.com>
2023-04-10 21:53:44 -0300

    Correção

--- estetica/settings.py
+++ estetica/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
