7995ff448aa3be3ab3c28d4728684d0aeefa427d
engdanilo <danilo.eng.mec@hotmail.com>
2023-04-11 22:05:48 -0300

    Cópia de staticfiles de webrafaela

--- estetica/settings2.py
+++ estetica/settings2.py
@@ -133,9 +133,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-#DISABLE_COLLECTSTATIC = 0
 #STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 # Default primary key field type
